27649924a0a667e35dfa270d9019dda0fc8cf2da
marijacurcic7 <52069615+marijacurcic7@users.noreply.github.com>
2019-07-03 22:09:38 +0200

    meni

--- Projekat/src/Main/MainFrame.java
+++ Projekat/src/Main/MainFrame.java
@@ -6,13 +6,16 @@ import java.awt.Toolkit;
 import java.util.Locale;
 import javax.swing.*;
 
+import View.Menu;
+
 //dfghjk
 // askdjsad
 
 public class MainFrame extends JFrame {
+	
 	private static MainFrame instance = null;
 	public static final String APP_TITLE = "Web Shop";
-
+	private Menu menu;
 
 	private MainFrame()	{
 		Locale.setDefault(new Locale("sr","RS"));
@@ -33,7 +36,8 @@ public class MainFrame extends JFrame {
         int screenHeight = screenSize.height;
         int screenWidth = screenSize.width;
         setSize(screenWidth *3/ 4, screenHeight *3/ 4);
-    
+        menu = new Menu();
+		setJMenuBar(menu);
        
 		
         setDefaultCloseOperation(EXIT_ON_CLOSE);
